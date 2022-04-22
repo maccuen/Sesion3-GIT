ac9c45c1b999633375c7eb6d7fd4c28ac670cd94
Macarena <maccuen@hotmail.com>
2022-04-22 20:25:55 +0200

    Initial Commit

---
+++ Sesion3-GIT/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sesion3-GIT
+//
+//  Created by Macarena Cuenca Carbajo on 22/4/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
